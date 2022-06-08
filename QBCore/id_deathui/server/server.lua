3361e105095936ee16b1bd1b9d9f3846fde4c205
Asmoddy <83936914+Asmoddy@users.noreply.github.com>
2022-06-09 02:47:30 +0800

    Player muted when dead

--- QBCore/id_deathui/server/server.lua
+++ QBCore/id_deathui/server/server.lua
@@ -1,9 +1,21 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterNetEvent('id_deathui:server:SetDeathStatus', function(isDead)
+RegisterNetEvent('hospital:server:SetLaststandStatus', function(isDead)
 	local src = source
-	local Player = QBCore.Functions.GetPlayer(src)
-	if Player then
-		Player.Functions.SetMetaData("isdead", isDead)
+	TriggerClientEvent('id_deathui:client:onDeath', src, isDead)
+	if isDead then
+		local isMuted = MumbleIsPlayerMuted(src)
+		if isMuted then return end
+		Player(src).state.muted = true
+		MumbleSetPlayerMuted(src, true)
 	end
+end)
+
+RegisterNetEvent('id_deathui:server:UnmuteSync')
+AddEventHandler('id_deathui:server:UnmuteSync', function()
+	local src = source
+	local isMuted = MumbleIsPlayerMuted(src)
+	if not isMuted then return end
+	Player(src).state.muted = false
+	MumbleSetPlayerMuted(src, false)
 end)
